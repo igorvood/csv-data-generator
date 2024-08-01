5694a841db7d5c17ee2c62173567dd916a0f1337
vood <igor-vood@gmail.com>
2024-08-01 19:39:53 +0300

    init

--- src/test/kotlin/ru/vood/bigdata/generatorcsv/dto/ClientTest.kt
+++ src/test/kotlin/ru/vood/bigdata/generatorcsv/dto/ClientTest.kt
@@ -3,6 +3,7 @@ package ru.vood.bigdata.generatorcsv.dto
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Assertions
+import java.io.File
 
 class ClientTest : FunSpec({
 
@@ -20,6 +21,25 @@ class ClientTest : FunSpec({
         (1..10)
             .map {  Client(it.toString())}
             .forEach { println(it.toString()) }
+
+    }
+
+    test("generate 1000 clients to file") {
+
+        val file = File("fileName")
+
+        val joinToString = (1..10_000_000)
+
+        file.printWriter().use { out ->
+            (1..1_000_000_000)
+                .map{Client(it.toString()).toString()}
+                .chunked(1000)
+                .map { asdasd ->  asdasd.joinToString                    ("\n")}
+                .forEach { clientStr ->
+                out.println(clientStr)
+            }
+        }
+
 
     }
 
