aa0a47fb4c746fef16e286e74a50fd5e06e3b91f
vood <igor-vood@gmail.com>
2024-08-01 18:16:55 +0300

    init

---
+++ src/test/kotlin/ru/vood/bigdata/generatorcsv/GeneratorCsvApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.vood.bigdata.generatorcsv
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GeneratorCsvApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
