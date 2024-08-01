7247cf543fb6ac02a2fb8151cd0659e833dc9782
vood <igor-vood@gmail.com>
2024-08-01 22:08:54 +0300

    init

--- src/main/kotlin/ru/vood/bigdata/generatorcsv/gen/EntityTemplate.kt
+++ src/main/kotlin/ru/vood/bigdata/generatorcsv/gen/EntityTemplate.kt
@@ -13,7 +13,7 @@ abstract class EntityTemplate<ID_TYPE>(
 //    id: ID_TYPE,
 ) : DataType<EntityTemplate<ID_TYPE>> {
 
-    private val meta: TreeMap<String, GenerateValueFunction<ID_TYPE, DataType<*>>> = TreeMap()
+    private val meta: TreeMap<String, MetaProperty<ID_TYPE, *>> = TreeMap()
 
 //    val id: DataType<ID_TYPE> = object : DataType<ID_TYPE> {
 //        override fun invoke(): ID_TYPE = id
@@ -22,7 +22,7 @@ abstract class EntityTemplate<ID_TYPE>(
     fun toString(id: ID_TYPE): String {
         val generate = generate(id) { entityTemplate, idVal ->
             entityTemplate.meta.map {
-                val value = it.value(idVal, it.key)()
+                val value = it.value.function(idVal, it.key)()
                 it.key + "=" + value
             }
                 .joinToString(", ")
@@ -37,8 +37,8 @@ abstract class EntityTemplate<ID_TYPE>(
         return printFun(this, id)
     }
 
-    internal fun addProp(build: MetaProperty<ID_TYPE, *>) {
-        meta[build.paramName] = build.function
+    internal fun addProp(metaProperty: MetaProperty<ID_TYPE, *>) {
+        meta[metaProperty.paramName] = metaProperty
     }
 
     fun string() = PropBuilder<String>()
