d50d8cac6d25acdd211efcf31acd7e1332b24eab
vood <igor-vood@gmail.com>
2024-08-02 19:43:53 +0300

    init

--- src/test/kotlin/ru/vood/bigdata/generatorcsv/dto/ClientTest.kt
+++ src/test/kotlin/ru/vood/bigdata/generatorcsv/dto/ClientTest.kt
@@ -3,10 +3,7 @@ package ru.vood.bigdata.generatorcsv.dto
 import io.kotest.common.runBlocking
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.reduce
-import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.flow.*
 import org.junit.jupiter.api.Assertions
 import java.io.File
 
@@ -20,28 +17,31 @@ class ClientTest : FunSpec({
 
 //    val client = Client()
     test("pseudo random test, not equals with dif Id") {
-        Assertions.assertNotEquals(Client("1").toString(), Client("2").toString())
+        Assertions.assertNotEquals(Client("1").myToString(), Client("2").myToString())
     }
 
     test("generate 10 clients") {
         (1..10)
-            .map { Client(it.toString()).toString() }
+            .map { Client(it.toString()).myToString() }
             .forEach { println(it) }
     }
-val cnt = 1000
-    test("generate $cnt clients to file") {
+    val cnt = 100
+    val subCnt = 100000
+    test("generate ${cnt * subCnt} clients to file") {
         val file = File("fileName")
         file.printWriter().use { out ->
 
-            for (i in 0 ..cnt){
-                val subCnt = 1000
+            for (i in 0..cnt) {
+
                 val runBlocking = runBlocking {
                     val map = (i * subCnt..(i * subCnt) + subCnt - 1)
                         .asFlow()
-                        .map { qwe -> Client(qwe.toString()).myToString() }
+                        .chunked(200)
+                        .map { qwe -> qwe.map { Client(it.toString()).myToString() } }
+//                        .reduce { accumulator, value -> accumulator + System.lineSeparator() + value }
                         .toList()
                         .joinToString(System.lineSeparator())
-//                        .reduce { accumulator, value -> accumulator + System.lineSeparator() + value }
+
                     map
                 }
                 out.println(runBlocking)
@@ -60,3 +60,19 @@ val cnt = 1000
     }
 
 })
+
+fun <T> Flow<T>.chunked(chunkSize: Int): Flow<List<T>> {
+    val buffer = mutableListOf<T>()
+    return flow {
+        this@chunked.collect {
+            buffer.add(it)
+            if (buffer.size == chunkSize) {
+                emit(buffer.toList())
+                buffer.clear()
+            }
+        }
+        if (buffer.isNotEmpty()) {
+            emit(buffer.toList())
+        }
+    }
+}
