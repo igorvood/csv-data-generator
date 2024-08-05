5922c7c180777fd10ae0b9fdb9ab7cf2208ac409
vood <igor-vood@gmail.com>
2024-08-05 11:56:05 +0300

    init

--- src/main/kotlin/ru/vood/bigdata/generatorcsv/gen/dsl/Alias.kt
+++ src/main/kotlin/ru/vood/bigdata/generatorcsv/gen/dsl/Alias.kt
@@ -3,11 +3,7 @@ package ru.vood.bigdata.generatorcsv.gen.dsl
 import ru.vood.bigdata.generatorcsv.gen.DataType
 import ru.vood.bigdata.generatorcsv.gen.EntityTemplate
 
-typealias GenerateIdValueFunction<T> = () -> DataType<T>
 typealias GenerateValueFunction<ID_TYPE, OUT_TYPE> = (id: ID_TYPE, paramName: String) -> OUT_TYPE
-//typealias GenerateFieldValueFunction<ID_TYPE, OUT_TYPE> = (EntityTemplate<ID_TYPE>, String) -> OUT_TYPE
-typealias GenerateFieldValueFunctionDsl<ID_TYPE, OUT_TYPE> = (EntityTemplate<ID_TYPE>, String) -> OUT_TYPE
-typealias GenerateFieldCheckFunction<ID_TYPE> = (EntityTemplate<ID_TYPE>, String) -> Boolean
 
 typealias ConstraintName = String
 typealias FieldName = String
