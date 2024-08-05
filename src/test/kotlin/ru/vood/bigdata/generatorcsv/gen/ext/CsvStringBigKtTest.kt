5f21d7edca47083b74e7a49500fa4c5f513f5a54
vood <igor-vood@gmail.com>
2024-08-05 21:06:42 +0300

    init

--- src/test/kotlin/ru/vood/bigdata/generatorcsv/gen/ext/CsvStringBigKtTest.kt
+++ src/test/kotlin/ru/vood/bigdata/generatorcsv/gen/ext/CsvStringBigKtTest.kt
@@ -3,6 +3,7 @@ package ru.vood.bigdata.generatorcsv.gen.ext
 import io.kotest.core.spec.style.FunSpec
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
@@ -11,7 +12,7 @@ import ru.vood.bigdata.generatorcsv.gen.EntityTemplate
 import java.io.File
 
 class CsvStringBigKtTest : FunSpec({
-    val cnt = 10_000_000_000
+    val cnt = 1_000_000_000
     val fileClients = "clients"
     val fileAcc = "acc"
     val foldCl = "w/${fileClients}.csv"
@@ -25,20 +26,32 @@ class CsvStringBigKtTest : FunSpec({
             val accontFlow = clientFlow
                 .map { it.accont(it.id()) }
 
-            val launch = async {
-                extracted(
-                    clientFlow,
-                    File("${foldCl}_paralel"), { it.csvStringBig() },
-                )
-            }
-            val launch1 = async {
-                extracted(
-                    accontFlow,
-                    File("${foldAcc}_paralel"), EntityTemplate<Client>::csvStringBig
-                )
-            }
-            launch.await()
-            launch1.await()
+            val asyncs = listOf(
+                async {
+                    extracted(
+                        clientFlow,
+                        File("${foldCl}_paralel"), { it.csvStringBig() },
+                    )
+                },
+                        async {
+                    extracted(
+                        accontFlow,
+                        File("${foldAcc}_paralel"), EntityTemplate<Client>::csvStringBig
+                    )
+                }
+            )
+
+//            val map = (1..50)
+//                .map {
+//                    async {
+//                        extracted(
+//                            clientFlow,
+//                            File("${foldCl}${it}_paralel"), { it.csvStringBig() },
+//                        )
+//                    }
+//                }
+//            map.awaitAll()
+            asyncs.awaitAll()
         }
 
     }
