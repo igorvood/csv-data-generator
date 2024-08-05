364aa4072d82f7360283d605827586a67dcd08eb
vood <igor-vood@gmail.com>
2024-08-05 18:46:42 +0300

    init

--- src/main/kotlin/ru/vood/bigdata/generatorcsv/gen/ext/extracted.kt
+++ src/main/kotlin/ru/vood/bigdata/generatorcsv/gen/ext/extracted.kt
@@ -1,9 +1,7 @@
 package ru.vood.bigdata.generatorcsv.gen.ext
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.flow.*
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import ru.vood.bigdata.generatorcsv.gen.EntityTemplate
 import java.io.File
@@ -16,17 +14,43 @@ suspend inline fun <reified ID_TYPE> extracted(
     crossinline f: (EntityTemplate<ID_TYPE>) -> String
 ) {
     val log = LoggerFactory.getLogger(ID_TYPE::class.java)
-    val everyRecordLog: Int = 2
+    val everyRecordLog: Int = 10000
     var num: Long = 0
-    flow
-        .map { cl -> f(cl) }
-        .collect {
-            file.printWriter().use { out ->
+    file.printWriter().use { out ->
+        flow
+            .map { cl -> f(cl) }
+            .collect {
+
                 out.println(it)
+
+                num += 1
+                if ((num % everyRecordLog).toInt() == 0) {
+                    log.info("Processed $num records")
+                }
             }
-            num += 1
-            if ((num % everyRecordLog).toInt() == 0){
-                log.info("Processed $num records")
-            }
-        }
+    }
+}
+
+ fun <ID_TYPE> extractedL(
+    flow: List<EntityTemplate<ID_TYPE>>,
+    file: File,
+    f: (EntityTemplate<ID_TYPE>) -> String,
+//    cl: Logger
+) {
+    val printWriter = file.printWriter()
+    val log: Logger = LoggerFactory.getLogger(EntityTemplate::class.java)
+    val everyRecordLog: Int = 10000
+    var num: Long = 0
+     printWriter.use { out ->
+         for (el in flow) {
+             val f1 = f(el)
+
+             out.println(f1)
+
+             num += 1
+             if ((num % everyRecordLog).toInt() == 0) {
+                 log.info("Processed $num records")
+             }
+         }
+     }
 }
--- src/test/kotlin/ru/vood/bigdata/generatorcsv/gen/ext/CsvStringBigKtTest.kt
+++ src/test/kotlin/ru/vood/bigdata/generatorcsv/gen/ext/CsvStringBigKtTest.kt
@@ -1,45 +1,46 @@
 package ru.vood.bigdata.generatorcsv.gen.ext
 
 import io.kotest.core.spec.style.FunSpec
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.withContext
 import ru.vood.bigdata.generatorcsv.dto.Client
 import ru.vood.bigdata.generatorcsv.gen.EntityTemplate
 import java.io.File
 
 class CsvStringBigKtTest : FunSpec({
     val cnt = 100
-    val subCnt = 10_000
     val fileClients = "clients"
     val fileAcc = "acc"
     val foldCl = "w/${fileClients}.csv"
     val foldAcc = "w/${fileAcc}.csv"
 
     test("csvStringBig") {
-        val clientFlow = (0..cnt).asFlow()
-            .map { Client(it.toString()) }
-//            .chunked(subCnt)
+//        withContext(Dispatchers.IO) {
+            val clientFlow = (0..cnt).asFlow()
+                .map { Client(it.toString()) }
 
-        val accontFlow = clientFlow
-            .map { it.accont(it.id()) }
-//            .map { it.map { it.accont(it.id()) } }
+            val accontFlow = clientFlow
+                .map { it.accont(it.id()) }
 
-        val launch = async {
-            extracted(
-                clientFlow,
-                File("${foldCl}_paralel"), EntityTemplate<String>::csvStringBig
-            )
+//            val launch = async {
+        extracted(
+                    clientFlow,
+                    File("${foldCl}_paralel"), {it.csvStringBig()},
+                )
+//            }
+//            val launch1 = async {
+        extracted(
+                    accontFlow,
+                    File("${foldAcc}_paralel"), EntityTemplate<Client>::csvStringBig
+                )
+//            }
+//            launch.await()
+//            launch1.await()
         }
-        val launch1 = async {
-            extracted(
-                accontFlow,
-                File("${foldAcc}_paralel"), EntityTemplate<Client>::csvStringBig
-            )
-        }
-        launch.await()
-        launch1.await()
 
-
-    }
+//    }
 })
