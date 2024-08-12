78bff8379126f6b4c77c3279d457448711059cae
vood <igor-vood@gmail.com>
2024-08-12 16:03:09 +0300

    init

--- app-generator/src/test/kotlin/ru/vood/bigdata/generatorcsv/gen/ext/JsonStringBigKtTest.kt
+++ app-generator/src/test/kotlin/ru/vood/bigdata/generatorcsv/gen/ext/JsonStringBigKtTest.kt
@@ -1,16 +1,7 @@
 package ru.vood.bigdata.generatorcsv.gen.ext
 
-import io.kotest.assertions.print.print
 import io.kotest.core.spec.style.FunSpec
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.withContext
 import ru.vood.bigdata.generatorcsv.dto.Client
-import ru.vood.bigdata.generatorcsv.gen.EntityTemplate
-import java.io.File
 
 class JsonStringBigKtTest : FunSpec({
     val cnt = 1_000//_000//_000
@@ -22,8 +13,7 @@ class JsonStringBigKtTest : FunSpec({
     test("jsonStringBig") {
 
 
-            println(Client("asdfgdfsgsdfgdfg").jsonString())
-
+        println(Client("asdfgdfsgsdfgdfg").jsonString())
 
 
     }
